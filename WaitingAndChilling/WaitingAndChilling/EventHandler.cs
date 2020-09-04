53c7193ccebb737301ab138be37ac52d9cb63655
F4Fridey <f4fridey@gmail.com>
2020-09-04 18:45:26 +0200

    successfully updated

---
+++ WaitingAndChilling/WaitingAndChilling/EventHandler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Smod2;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using UnityEngine;
+
+namespace WaitingAndChilling
+{
+    class EventHandler : IEventHandlerFixedUpdate, IEventHandlerPlayerJoin, IEventHandlerRoundRestart, IEventHandlerRoundStart
+    {
+        private readonly WaitingAndChilling plugin;
+
+        bool waitingForPlayers = true;
+
+        float time = 1;
+        float timer = 0;
+
+        public EventHandler(WaitingAndChilling plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public void OnFixedUpdate(FixedUpdateEvent ev)
+        {
+            if (waitingForPlayers)
+            {
+                try { GameObject.Find("StartRound").transform.localScale = Vector3.zero; }
+                catch { plugin.Debug("StartRound not found... If you see this because you just started the server, ignore this."); }
+
+                if (timer < time)
+                {
+                    timer += Time.deltaTime;
+                }
+                else if (plugin.Server.GetPlayers().Count > 0)
+                {
+                    timer = 0;
+                    string str = "<color=#fffbad>WAITING FOR PLAYERS</color>\n";
+                    short waitingTimer = GameCore.RoundStart.singleton.Timer;
+                    if (waitingTimer == -2)
+                    {
+                        str += "<color=#ff7d92>paused</color>";
+                    }
+                    else if (waitingTimer == -1)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        str += "<color=#7dff8a>" + waitingTimer.ToString("0") + "</color> seconds left";
+                    }
+
+                    foreach (ReferenceHub referenceHub in ReferenceHub.GetAllHubs().Values)
+                    {
+                        if (referenceHub.isDedicatedServer)
+                            continue;
+                        referenceHub.hints.Show(new Hints.TextHint(str, new Hints.HintParameter[] { new Hints.StringHintParameter(string.Empty) }, Hints.HintEffectPresets.FadeInAndOut(0f), 1f));
+                    }
+                }
+                else
+                {
+                    timer = 0;
+                }
+            }
+        }
+
+        public void OnPlayerJoin(PlayerJoinEvent ev)
+        {
+            if (waitingForPlayers)
+            {
+                ev.Player.ChangeRole(Smod2.API.RoleType.TUTORIAL, true, false);
+                ev.Player.Teleport(new Vector(53, 1020, -43));
+                plugin.Info("Set " + ev.Player.Name + " to tutorial");
+            }
+        }
+
+        public void OnRoundRestart(RoundRestartEvent ev)
+        {
+            waitingForPlayers = true;
+        }
+
+        public void OnRoundStart(RoundStartEvent ev)
+        {
+            waitingForPlayers = false;
+        }
+    }
+}
---
+++ WaitingAndChilling/WaitingAndChilling/WaitingAndChilling.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Smod2;
+using Smod2.API;
+using Smod2.Attributes;
+using Smod2.Config;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using Smod2.Lang;
+using Smod2.Piping;
+
+namespace WaitingAndChilling
+{
+    [PluginDetails(
+        author = "F4Fridey",
+        name = "Waiting And Chilling",
+        description = "A plugin that spawn players in during the Waiting For Players stage.",
+        id = "f4fridey.waitingandchilling.plugin",
+        configPrefix = "wac",
+        langFile = "waitingandchilling",
+        version = "1.3",
+        SmodMajor = 3,
+        SmodMinor = 8,
+        SmodRevision = 4
+        )]
+    public class WaitingAndChilling : Plugin
+    {
+        [ConfigOption]
+        public readonly bool enabled = true;
+
+        public override void OnDisable()
+        {
+            Info("WAC Disabled.");
+        }
+
+        public override void OnEnable()
+        {
+            if (enabled)
+            {
+                Info("WAC Enabled.");
+            }
+            else
+            {
+                Info("WAC has not registered, wac_enabled is false!");
+            }
+        }
+
+        public override void Register()
+        {
+            if (enabled)
+            {
+                AddEventHandlers(new EventHandler(this));
+            }
+        }
+    }
+}
